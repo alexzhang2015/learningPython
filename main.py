54ba445ba8b48ad6c6fc88511289e6dfa3dc200f
alex <chenhua.zhang@ele.me>
2015-10-29 11:28:21 +0800

    use the thread queue module

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ import re
 from datetime import datetime
 import csv
 import copy
+from threading import Thread
+from Queue import Queue
 
 
 header_info = {
@@ -26,7 +28,7 @@ configure = {}
 
 # task URLS
 spider_tasks = []
-spider_tasks_queue = []
+spider_tasks_queue = Queue()
 
 # result queue
 result_queue = []
@@ -41,52 +43,48 @@ def load_configure():
 
 def init_task_urls():
     # "http://news.163.com/"
-    global spider_tasks
+    # global spider_tasks
     spider_tasks.append("http://news.163.com/")
-    spider_tasks_queue.append("http://news.163.com/")
+    spider_tasks_queue.put("http://news.163.com/")
 
 
-def generate_url_task(thread_name):
-    global spider_tasks_queue
-    global spider_tasks
+# task generation
+def generate_url_task(thread_name, task_queue):
+    # global spider_tasks
 
     while True:
-        spider_tasks_queue = copy.deepcopy(spider_tasks)
+        for task_url in spider_tasks:
+            task_queue.put(task_url)
+
         time_interval = 10
         time.sleep(time_interval)
 
 
-def spider_task_thread(thread_name):
-    global spider_tasks_queue
-    global result_queue
-
+def spider_task_thread(thread_name, task_queue):
+    # global result_queue
     while True:
-        if spider_tasks_queue:
-            task_url = spider_tasks_queue.pop()
-            http_handle = requests.get(task_url, headers=header_info, timeout=60)
-            if http_handle.status_code == 200:
-                data = http_handle.text
-                #     <a class="ac01" href="http://news.163.com/15/1026/18/B6SEGPVC00014JB5.html">test</a>
-                # TODO: add the regular express to configure.json
-                match = re.findall(r'<a class="ac01" href="(.*?)".*>(.*)</a>', data)
-                if match:
-                    for link, title in match:
-                        print datetime.now().isoformat()
-                        print "%s -> %s" % (link, title)
-                        # date, link, title, source
-                        result_data = [datetime.now().isoformat(), link, title.encode("utf-8"), task_url]
-                        result_queue.append(result_data)
-            else:
-                print "response code:", http_handle.status_code
-
-        # TODO: use the configure.json
-        time_interval = 10
-        time.sleep(time_interval)
+        task_url= task_queue.get()
+        http_handle = requests.get(task_url, headers=header_info, timeout=60)
+        if http_handle.status_code == 200:
+            data = http_handle.text
+            #     <a class="ac01" href="http://news.163.com/15/1026/18/B6SEGPVC00014JB5.html">test</a>
+            # TODO: add the regular express to configure.json
+            match = re.findall(r'<a class="ac01" href="(.*?)".*>(.*)</a>', data)
+            if match:
+                for link, title in match:
+                    print datetime.now().isoformat()
+                    print "%s -> %s" % (link, title)
+                    # date, link, title, source
+                    result_data = [datetime.now().isoformat(), link, title.encode("utf-8"), task_url]
+                    result_queue.append(result_data)
+        else:
+            print "response code:", http_handle.status_code
+        task_queue.task_done()
 
 
 # TODO: add the Persistence to topnews.csv
 def save_to_csv_task(thread_name):
-    global result_queue
+    # global result_queue
     while True:
         time.sleep(10)
         if result_queue:
@@ -105,20 +103,21 @@ if __name__ == '__main__':
 
     # 2. init the task URLS
     init_task_urls()
-    thread.start_new_thread(generate_url_task , ("generate_url_task",))
+    thread.start_new_thread(generate_url_task , ("generate_url_task",spider_tasks_queue,))
 
     # 3. sync thread
     thread.start_new_thread(save_to_csv_task , ("save_to_csv_task",))
 
-    # 4. worker thread
-    try:
-        # TODO: use the configure.json
-        THREAD_NUMBER = 4
-        for i in range(THREAD_NUMBER):
-            thread.start_new_thread(spider_task_thread , ("spider_task",))
-    except:
-       print "Error: unable to start thread"
+    # 4. consumer worker thread
+    WORKERS = 4
 
-    # main thread
-    while True:
-        time.sleep(60)
+    # Set up some threads to fetch the enclosures
+    for i in range(WORKERS):
+        worker = Thread(target=spider_task_thread, args=("spider_task", spider_tasks_queue,))
+        worker.setDaemon(True)
+        worker.start()
+
+    # join the main thread
+    spider_tasks_queue.join()
+
+
